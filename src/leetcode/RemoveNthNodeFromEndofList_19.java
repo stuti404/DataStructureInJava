62f99e01204e2a45ac237c01b78fce790d6c9528
stuti404 <83167129+stuti404@users.noreply.github.com>
2023-02-19 22:40:23 +0530

    ListNode

---
+++ src/leetcode/RemoveNthNodeFromEndofList_19.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+public class RemoveNthNodeFromEndofList_19 {
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+       public ListNode removeNthFromEnd(ListNode head, int n) {
+            ListNode fast = head;
+            ListNode slow = head;
+
+            while (n-->0) fast = fast.next;
+            while (fast != null && fast.next!=null){
+                slow = slow.next;
+                fast = fast.next;
+            }
+            if (slow == null || slow.next == null) return null;
+            else if (slow==head && fast==null) head= head.next;
+            else slow.next = slow.next.next;
+            return head;
+        }
+
+}
