5292b73b7a80a71a0d3cbee8220f81c3fa4a0684
stuti404 <83167129+stuti404@users.noreply.github.com>
2023-02-20 00:02:43 +0530

    tree

---
+++ src/leetcode/PopulatingNextRightPointersinEachNode_116.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+public class PopulatingNextRightPointersinEachNode_116 {
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    };
+    public Node connect(Node root) {
+        Node black = root;
+        while (black !=null && black.left != null){
+            Node n = black;
+            while (true){
+                n.left.next = n.right;
+                if (n.next == null) break;
+                n.right.next = n.next.left;
+                n= n.next;
+            }
+            black = black.left;
+        }
+        return root;
+    }
+}
