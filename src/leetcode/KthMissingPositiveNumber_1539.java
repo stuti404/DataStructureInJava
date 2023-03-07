d7b7f691223fd1daad025b64a616c4626657e005
stuti404 <83167129+stuti404@users.noreply.github.com>
2023-03-07 20:50:19 +0530

    binary search

---
+++ src/leetcode/KthMissingPositiveNumber_1539.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class KthMissingPositiveNumber_1539 {
+    public int findKthPositive(int[] arr, int k) {
+        int n = arr.length;
+        int left =0, right =n-1;
+        int missing = compute(arr[n-1],n);
+        while (left<=right){
+            int mid = left+(right-left)/2;
+            missing=compute(arr[mid],mid+1);
+            if (missing >=k) right= mid-1;
+            else left = mid+1;
+        }
+        if (right==-1) return k;
+        return arr[right]+k-compute(arr[right],right+1);
+    }
+    public static  int compute(int actual , int expected ){
+        return  actual-expected;
+    }
+}
