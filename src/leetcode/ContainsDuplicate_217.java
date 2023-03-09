e0c04566a2753b0ffb1638fd0760b473e4c83c42
stuti404 <83167129+stuti404@users.noreply.github.com>
2023-03-09 23:08:10 +0530

    Arrays

---
+++ src/leetcode/ContainsDuplicate_217.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class ContainsDuplicate_217 {
+    public static void main(String[] args) {
+        int []nums = {1,2,3,4};
+        System.out.println(containsDuplicate(nums));
+    }
+    public static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length-1; i++) {
+            if (nums[i]==nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
